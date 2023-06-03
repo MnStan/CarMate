db8a1e2184323d264c17dd274e6c2fe7226df58c
MnStan <mmaks97@gmail.com>
2023-06-03 10:07:09 +0200

    Added unlogged main page

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ $path = parse_url($path, PHP_URL_PATH);
 Routing::get('', 'DefaultController');
 Routing::get('login', 'DefaultController');
 Routing::get('register', 'DefaultController');
+Routing::get('unloggedMain', 'DefaultController');
 Routing::get('cars', 'DefaultController');
 Routing::run($path);
