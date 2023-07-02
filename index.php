10ab984b01c97eedc1a00521814381af0d4e71fe
MnStan <mmaks97@gmail.com>
2023-07-02 14:11:28 +0200

    Add AppController

--- index.php
+++ index.php
@@ -3,15 +3,28 @@
 require 'Routing.php';
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
-$path = parse_url($path, PHP_URL_PATH);
+$base = parse_url($path, PHP_URL_PATH);
+$query = parse_url($path, PHP_URL_QUERY);
 
-Routing::get('', 'DefaultController');
-Routing::get('login', 'DefaultController');
-Routing::get('register', 'DefaultController');
 Routing::get('unloggedMain', 'DefaultController');
-Routing::get('main', 'DefaultController');
 Routing::get('info', 'DefaultController');
 Routing::get('bookCar', 'DefaultController');
-Routing::get('addCar', 'DefaultController');
 Routing::get('rentDone', 'DefaultController');
-Routing::run($path);
+
+Routing::get('', 'CarController');
+Routing::get('addCar', 'CarController');
+Routing::post('addCarForm', 'CarController');
+Routing::get('main', 'CarController');
+
+Routing::get('login', 'DefaultController');
+Routing::get('register', 'DefaultController');
+Routing::get('statute', 'DefaultController');
+Routing::get('contact', 'DefaultController');
+Routing::get('privacyPolicy', 'DefaultController');
+
+Routing::post('checkLogin', 'SecurityController');
+Routing::post('checkRegister', 'SecurityController');
+
+Routing::post('logout', 'SessionController');
+
+Routing::run($base, $query);
