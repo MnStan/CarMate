d75de17f743407ee9f29ad9e60ee817a344ef63f
MnStan <58117854+MnStan@users.noreply.github.com>
2023-09-09 12:48:45 +0200

    Develop (#18)
    * Project setup
    
    * Added routing (#1)
    
    * Added login and register page (#2)
    
    * Added unlogged main page (#3)
    
    * Feature/adding navbar (#4)
    
    * Added navbar
    
    * Fixed logo position in navbar
    
    * Added main page with car-card components and card component for car searching (#5)
    
    * Added carInfo page + bean component for infomations about car and rent and added page with datepicker (#6)
    
    * Feature/adding add car page (#7)
    
    * Added addCar page template and css file
    
    * Added button to add photo with .js script and preview
    
    * Added buttons to add informations about car and rent
    
    * Added redirect button (#8)
    
    * Added link for logo (#9)
    
    * Added database connection (#10)
    
    * Feature/controllers (#11)
    
    * Add AppController
    
    * Added session controller
    
    * Added controllers
    
    * Changed styling for register page (#12)
    
    * Feature/add page (#13)
    
    * Changed addCarPage and register
    
    * Register page done. Added input verification, changed page layout
    
    * Changed car information page. Added photos, buttons for photos and modal presentation of information about car rent (#14)
    
    * Resolved bug with unloggedMain and main page
    
    * Added statute, contact and privacy politicy pages (#15)
    
    * Implemented searching by city or car name (#16)
    
    * Test data with pictures added
    
    * Update README.md
    
    * a

--- public/views/components/headImports.php
+++ public/views/components/headImports.php
@@ -2,6 +2,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="public/css/style.css">
+<script src="https://kit.fontawesome.com/4aadd4b874.js" crossorigin="anonymous"></script>
+<script src="public/js/select.js" defer></script>
+<script src="public/js/addCarAlerts.js" defer></script>
+<script src="public/js/validateRegister.js"defer></script>
+<script src="public/js/car-interactions.js"defer></script>
 <script src="public/js/select.js" defer></script>
 <script src="public/js/addCarAlerts.js" defer></script>
 <script src="public/js/validateRegister.js"defer></script>
